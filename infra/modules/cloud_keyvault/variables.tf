eaeea851d014e3d5025b58661228608f31a7275b
WinwireSreeNavya <sreenavya.kln@winwire.com>
2022-11-28 14:37:36 +0530

    Create variables.tf

---
+++ infra/modules/cloud_keyvault/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "base name of any resource"
+}
+
+variable "keyvault_name" {
+  type        = string
+  description = "name of key vault"
+}

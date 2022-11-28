e434b4673974e422548209bc18e953ef21f982fe
WinwireSreeNavya <sreenavya.kln@winwire.com>
2022-11-28 14:36:56 +0530

    Update main.tf

--- infra/modules/cloud_keyvault/main.tf
+++ infra/modules/cloud_keyvault/main.tf
@@ -1,43 +1,13 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.0"
-    }
-  }
-}
 data "azuread_client_config" "current" {}
 
-provider "azurerm" {
-  features {
-  resource "azurerm_key_vault" "kv1" {
-  name                        = "test_ww1"
+resource "azurerm_key_vault" "kv1" {
+  name                        = var.keyvault_name
   location                    = "East US"
-  resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
   enabled_for_disk_encryption = true
-
-  tenant_id                   = [data.azurerm_client_config.current.tenant_id]
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
-
-  access_policy {
-    object_id = [data.azurerm_client_config.current.object_id]
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
- }
-}
 }

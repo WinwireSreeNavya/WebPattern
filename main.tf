f52abba77461957388f0dca494ae304e719d53c2
WinwireSreeNavya <sreenavya.kln@winwire.com>
2022-12-14 15:32:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,11 @@ terraform {
 
 provider "azurerm" { 
   
-  version = "2.5.0"
+  client_id         = ARM_CLIENT_ID
+  client_secret     = ARM_CLIENT_SECRET
+  subscription_id   = ARM_SUBSCRIPTION_ID
+  tenant_id         = ARM_TENANT_ID
+  
   features {}
 }
 

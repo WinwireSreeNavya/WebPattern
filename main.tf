579513885514ce9c2c06f12b2105b4d1b9dee97d
WinwireSreeNavya <sreenavya.kln@winwire.com>
2022-12-15 15:54:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,8 +64,7 @@ resource "azurerm_mssql_database" "test" {
   name           = "exampledbtest-db-d"
   server_id      = azurerm_mssql_server.example.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  license_type   = "LicenseIncluded"  
   read_scale     = true
   sku_name       = "S0"  
 }

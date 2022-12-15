25d5773cc508ec992af46ac1d14b97dc1cb5c122
WinwireSreeNavya <sreenavya.kln@winwire.com>
2022-12-15 16:00:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,12 +59,3 @@ resource "azurerm_mssql_server" "example" {
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
-
-resource "azurerm_mssql_database" "test" {
-  name           = "exampledbtest-db-d"
-  server_id      = azurerm_mssql_server.example.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"  
-  read_scale     = true
-  sku_name       = "S0"  
-}

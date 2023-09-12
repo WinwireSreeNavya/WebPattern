da216c3bb2b8d10764adfb6b27bb72b30ed57ce9
WinwireSreeNavya <sreenavya.kln@winwire.com>
2023-09-12 20:07:00 +0530

    Commit for automation

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
 
-provider "azurerm" {  
-  
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
